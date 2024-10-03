ff01c9d617842e9f11ed76a73ce01675672c582b
Meric Capar <mericcaparrr@gmail.com>
2024-10-03 20:01:41 +0300

    Footer eklendi

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Sitemin sonuna da sevdiğim bir şarkıyı eklemek istedim. Ziyaret ettiğiniz için teşekkürler!</p>
+      <div className="video-container">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/x4LmWbxPBkE?autoplay=1&mute=1&controls=1" 
+          title="Sevdiğim Şarkı"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div className="footer-info">
+        <p>&copy; 2024 Meriç Çapar</p>
+        <p>+90 536 211 7030</p> 
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
